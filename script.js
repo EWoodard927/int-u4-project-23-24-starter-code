8b9593cb3b07a84cad648527949db6179ae4ed97
EWoodard927 <EWoodard27@gmail.com>
2026-06-29 12:44:30 -0500

    I added a fifth question, so that an all-way tie is impossible.

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ let purpleButton1 = document.querySelector(".purpleButton1");
 let purpleButton2 = document.querySelector(".purpleButton2");
 let purpleButton3 = document.querySelector(".purpleButton3");
 let purpleButton4 = document.querySelector(".purpleButton4");
+let purpleButton5 = document.querySelector(".purpleButton5");
 let greenButton1 = document.querySelector(".greenButton1");
 let greenButton2 = document.querySelector(".greenButton2");
 let greenButton3 = document.querySelector(".greenButton3");
 let greenButton4 = document.querySelector(".greenButton4");
+let greenButton5 = document.querySelector(".greenButton5");
 let blueButton1 = document.querySelector(".blueButton1");
 let blueButton2 = document.querySelector(".blueButton2");
 let blueButton3 = document.querySelector(".blueButton3");
 let blueButton4 = document.querySelector(".blueButton4");
+let blueButton5 = document.querySelector(".blueButton5");
 let yellowButton1 = document.querySelector(".yellowButton1");
 let yellowButton2 = document.querySelector(".yellowButton2");
 let yellowButton3 = document.querySelector(".yellowButton3");
 let yellowButton4 = document.querySelector(".yellowButton4");
+let yellowButton5 = document.querySelector(".yellowButton5");
 let submitButton = document.querySelector(".submitButton");
 let questions = document.querySelector(".questions");
 let questionOne = document.querySelector(".questionOne");
 let questionTwo = document.querySelector(".questionTwo");
 let questionThree = document.querySelector(".questionThree");
 let questionFour = document.querySelector(".questionFour");
+let questionFive = document.querySelector(".questionFive");
 let purpleScore = 0;
 let greenScore = 0;
 let blueScore = 0;
@@ -57,6 +62,12 @@ purpleButton3.addEventListener('click', function(){
 purpleButton4.addEventListener('click', function(){
     purpleScore += 1;
     questionFour.style.display = "none";
+    questionFive.style.display = "block";
+});
+
+purpleButton5.addEventListener('click', function(){
+    purpleScore += 1;
+    questionFive.style.display = "none";
     questions.style.display = "none";
     submitButton.style.display = "block";
 });
@@ -82,6 +93,12 @@ greenButton3.addEventListener('click', function(){
 greenButton4.addEventListener('click', function(){
     greenScore += 1;
     questionFour.style.display = "none";
+    questionFive.style.display = "block";
+});
+
+greenButton5.addEventListener('click', function(){
+    greenScore += 1;
+    questionFive.style.display = "none";
     questions.style.display = "none";
     submitButton.style.display = "block";
 });
@@ -107,6 +124,12 @@ blueButton3.addEventListener('click', function(){
 blueButton4.addEventListener('click', function(){
     blueScore += 1;
     questionFour.style.display = "none";
+    questionFive.style.display = "block";
+});
+
+blueButton5.addEventListener('click', function(){
+    blueScore += 1;
+    questionFive.style.display = "none";
     questions.style.display = "none";
     submitButton.style.display = "block";
 });
@@ -132,6 +155,12 @@ yellowButton3.addEventListener('click', function(){
 yellowButton4.addEventListener('click', function(){
     yellowScore += 1;
     questionFour.style.display = "none";
+    questionFive.style.display = "block";
+});
+
+yellowButton5.addEventListener('click', function(){
+    yellowScore += 1;
+    questionFive.style.display = "none";
     questions.style.display = "none";
     submitButton.style.display = "block";
 });
